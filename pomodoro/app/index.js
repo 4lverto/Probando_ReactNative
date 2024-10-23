90923771e136c77daf4c5c0637686d6095976d77
4lverto <kesolesito@gmail.com>
2024-10-23 19:50:25 +0200

    Feature pomodoro finalizada

--- pomodoro/app/index.js
+++ pomodoro/app/index.js
@@ -1,6 +1,7 @@
 // Imports de Funcionalidades / Bibliotecas
-import { StyleSheet, Platform, View, Text, Button } from 'react-native';
-import { useState } from 'react'; // Sirve para declarar variables
+import { StyleSheet, Platform, View, Text, Button, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react'; // Sirve para declarar variables
+import { Audio } from "expo-av";
 
 // Imports de componentes
 import Header from "../components/Header"
@@ -10,17 +11,69 @@ const colors = ["#F7DC6F","#A2D9CE","#D7BDE2"];
 
 export default function App() {
 
-  const [isWorkin, setIsWork] = useState(false);
+  const [isWorking, setIsWorking] = useState(false); // Lo único que hace es saber si el usuario está en alguno de los menús
   const [time, setTime] = useState(25 * 60);
   const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  {
+    /* Accedemos a los ciclos de vida de componentes  
+    {función callback}  [array de dependencias que representa un efecto que se activará si el contenido del array cambia] */ 
+  }
+  useEffect(() => {
+    let intervalo = null;
+
+    if(isActive){
+      // Run timer -> setInterval toma una función y cada cuanto tiempo (ms) queremos usarla
+      intervalo=setInterval(() => {
+        setTime(time-1);
+      },1000)
+    }else{
+      // Limpiar el intervalo -> 
+      clearInterval(intervalo); // Esto hará que cuando el reloj no esté activo lo quitaremos del fondo.
+    }
+    
+    if(time === 0){
+      setIsActive(false);
+      setIsWorking((prev) => !prev); // React tiene un problemilla con la actualización de los componentes.
+      setTime(isWorking ? 300 : 1500);
+      escucharSonido();
+    }
+
+    return() => clearInterval(intervalo);
+    // Si no usamos el array de dependencias de useEffect, este useEffect tan solo se ejecuta una vez cuando el componente se monta.
+    // Poniendo isActive lo que pasará será que cuando cambie isActive se ejecutará
+    // Si añadimos "time", también se interpretará que cada vez que "time" cambie también se ejecute. Sin embargo, sólo con esto no basta,
+    // por lo que añadimos el return => clearInterval(intervalo) para que no se vuelva loco y no se hagan varios intervalos..
+  }, [isActive,time])
+
+  function manejarStartStop(){
+    escucharSonido();
+    setIsActive(!isActive);
+  }
+
+  async function escucharSonido(){
+
+    const { sound } = await Audio.Sound.createAsync( require("../assets/audios/orb.mp3") );
+    
+    await sound.playAsync();
+  }
+
 
   return (
     <View style={[styles.container, {flex:1},{backgroundColor: colors[currentTime] }, {paddingHorizontal: 15}, ]}>
         <Text style={styles.text}> Pomodoro </Text>
         {/* Abstraemos toda la lógica de nuestro Header con esto: */}
         <Header currentTime={currentTime} setCurrentTime={setCurrentTime} setTime={setTime}/>
-        {/* */}
+        {/* Abstraemos la lógica correspondiente a la gestión del Timer*/}
         <Timer time={time}> </Timer>
+
+        {/* Botones para empezar y parar el timer */}
+        <TouchableOpacity onPress={manejarStartStop} style={styles.boton}>
+          <Text style={{color: "white" , fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity> 
     </View>
   );
 }
@@ -32,8 +85,19 @@ const styles = StyleSheet.create({
   },
 
   text: {
-    fontSize: 32,
+    padding: 15,
+    textDecorationLine: "underline",
+    textAlign: "center",
+    fontSize: 50,
     fontWeight: "bold"
-}
+},
+
+  boton: {
+    alignItems: "center",
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+  }
 
 });
--- pomodoro/components/Timer.jsx
+++ pomodoro/components/Timer.jsx
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet } from "react-native";
 
 export default function Timer({time}){
+
+    {/*Calculamos el tiempo en minutos a partir del tiempo en segundos que teníamos y le damos el formato que queremos: 05:00 (si quedan 5 minutos) */}
+    const tiempoFormateado = `${Math.floor(time/60).toString().padStart(2,"0")}:${(time % 60).toString().padStart(2,"0")}`;
+    
     return(
         <View style={styles.container}>
-            <Text style={styles.time}> {time} </Text>
+            <Text style={styles.time}> {tiempoFormateado} </Text>
         </View>
     );
 }
