a1690644b8c848637b950be5d23f38e99db0b619
4lverto <kesolesito@gmail.com>
2024-10-28 12:58:47 +0100

    Ultimas 5 imagenes almacenadas

--- Nasa/app/views/Home/Home.tsx
+++ Nasa/app/views/Home/Home.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import {View,StyleSheet}from 'react-native';
+import { format,sub } from 'date-fns';
 
 import fetchApi from '../../utils/fetch';
 import Header from '../../components/Header';
 import ImagenHoy from '../../components/ImagenHoy';
-import { PostImage } from '@/app/types';
+import { PostImage } from '../../types';
 
 const Home = () => {
 
 
     const [imagenHoy, setImagenHoy] = useState<PostImage>();
+
+    const [imagenesUltimos5Dias, setImagenesUltimos5Dias] = useState<PostImage[]>([]);
 
     /*La función useEffect sirve para que podamos ejecutar lo que sea que queramos ejecutar cuando cambien ciertas dependencias.
     Por ejemplo, querremos que se llame a la API cuando el componente se monte por primera vez */
@@ -26,13 +29,32 @@ const Home = () => {
             }
         };
 
+        // Para cargar el resto de imágenes (de los últimos 5 días)
+        const cargarUltimasImagenes5Dias = async () => {
+            try{
+                const date = new Date();
+                const fechaHoy = format(date,'yyyy-MM-dd');
+                const fechaHace5Dias = format(sub(date,{days: 5}),'yyyy-MM-dd');
+                
+                const fechaHace5DiasResponse = await fetchApi(`&start_date=${fechaHace5Dias}&end_date=${fechaHoy}`);
+                
+                setImagenesUltimos5Dias(fechaHace5DiasResponse);
+                
+            }catch(error){
+                console.error(error);
+            }
+        };
+
         // Llamamos a la función
         cargarImagenHoy().catch(null);
+        cargarUltimasImagenes5Dias().catch(null);
 
     } , []);
 
     // Mostramos lo que obtenemos de la llamada a la API en una constante de estado local
-    console.log(imagenHoy);
+    // (ya no hace falta porque sabemos que funciona)
+    // console.log(imagenHoy);
+    console.log(imagenesUltimos5Dias);
     
     // Colocamos nuestros componentes
     return (
